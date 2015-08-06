fd23b84596496c0049ca14fbc1135decf980dc29
Kier Davis <kierdavis@gmail.com>
2015-08-06 20:27:49 +0100

    Fix a couple of bugs in k12_alu.v (not k12_alu_ttl.v, surprisingly) that were made evident by the comparison between the outputs of the corresponding testbenches)

--- rtl/k12/k12_alu.v
+++ rtl/k12/k12_alu.v
@@ -8,34 +8,41 @@ module k12_alu(
 
     wire [7:0] bi = inst[12] ? inst[7:0] : b;
     
+    wire subtract = ~inst[11] | inst[8];
+    wire [7:0] adder_a = a;
+    wire [7:0] adder_b = subtract ? ~bi : bi;
+    wire [7:0] adder_result;
+    wire carry;
+    assign {carry, adder_result} = {1'd0, adder_a} + {1'd0, adder_b} + {8'd0, subtract};
+    
     wire [2:0] func = inst[10:8];
+    
     assign res = (func == 3'h0) ? a :
                  (func == 3'h1) ? (a & bi) :
                  (func == 3'h2) ? (a | bi) :
                  (func == 3'h3) ? (a ^ bi) :
-                 (func == 3'h4) ? (a + bi) :
-                 (func == 3'h5) ? (a - bi) :
+                 (func == 3'h4) ? adder_result :
+                 (func == 3'h5) ? adder_result :
                  (func == 3'h6) ? {a[7], a[7:1]} :
                  (func == 3'h7) ? bi : 8'hxx;
     
-    wire [7:0] cmp_b = ~bi;
-    wire [8:0] cmp_res = {1'd0, a} + {1'd0, cmp_b} + 9'd1;
-    
-    wire zero = cmp_res[7:0] == 8'd0;
-    wire negative = cmp_res[7];
-    wire borrow = ~cmp_res[8];
-    wire overflow = (a[7] ^ cmp_res[7]) & (cmp_b[7] ^ cmp_res[7]);
+    wire zero = adder_result == 8'd0;
+    wire negative = adder_result[7];
+    wire borrow = ~carry;
+    wire overflow = (adder_a[7] ^ adder_result[7]) & (adder_b[7] ^ adder_result[7]);
     wire ule = borrow | zero;
     wire slt = negative ^ overflow;
     wire sle = slt | zero;
     
-    assign cond = (func == 3'h0) ? zero :
-                  (func == 3'h1) ? negative :
-                  (func == 3'h2) ? borrow :
-                  (func == 3'h3) ? overflow :
-                  (func == 3'h4) ? borrow :
-                  (func == 3'h5) ? ule :
-                  (func == 3'h6) ? slt :
-                  (func == 3'h7) ? sle : 1'hx;
+    wire raw_cond = (func == 3'h0) ? zero :
+                    (func == 3'h1) ? negative :
+                    (func == 3'h2) ? borrow :
+                    (func == 3'h3) ? overflow :
+                    (func == 3'h4) ? borrow :
+                    (func == 3'h5) ? ule :
+                    (func == 3'h6) ? slt :
+                    (func == 3'h7) ? sle : 1'hx;
+    
+    assign cond = inst[13] ? ~raw_cond : raw_cond;
     
 endmodule
