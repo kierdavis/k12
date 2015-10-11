592fab499f9d9ab9d93ec49468df103fc2fc0db1
Kier Davis <kierdavis@gmail.com>
2015-10-11 20:35:48 +0100

    Implement stack I/O port in emulator (code was actually written on 18/08/2015

--- tools/go/src/emulator/port.go
+++ tools/go/src/emulator/port.go
@@ -21,3 +21,21 @@ type DebugOutputPort struct {}
 func (DebugOutputPort) Write(x uint8) {
 	fmt.Printf("OUT: dec %3d % 3d  hex %02X  char %q\n", x, int8(x), x, x)
 }
+
+// Implements an 8-bit stack.
+type Stack struct {
+    Data []uint8
+}
+
+func (stack *Stack) Read() (x uint8) {
+    n := len(stack)
+    if n > 0 {
+        x = stack.Data[n-1]
+        stack.Data = stack.Data[:n-1]
+    }
+    return x
+}
+
+func (stack *Stack) Write(x uint8) {
+    stack.Data = append(stack.Data, x)
+}
